430c0e876a6ca407d45aff079412b96303374e05
YoungJune Kang <cerealmaster@naver.com>
2023-02-13 14:32:39 +0000

    class component 에서 life cycle methods 종류 배움

--- frontend/react-demo/src/App.js
+++ frontend/react-demo/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log('constructor')
+    super();
+    this.state = {
+      names : []
+    }
+  }
+  
+  componentDidMount() {
+    console.log('didmount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res)=>res.json())
+      .then((users)=>{
+        this.setState(
+          ()=>{return ({names : users})},
+        ()=>{console.log(users)}
+        )
+      })
+  }
+
+  render() {
+    console.log('render')
+    return (
+      <div className = "App">
+        {this.state.names.map((m)=>{
+          return <div key={m.id}>
+            <h1>{m.name}</h1>
+          </div>;
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
